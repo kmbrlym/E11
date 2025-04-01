8941e9e2de11a2b356825455a4b68b0dc7d5f47a
Kimberly <kimberly.michela@mail.utoronto.ca>
2025-04-01 02:02:50 -0400

    e11

---
+++ my-next-app/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div id="auto-tester">
+      Hi. I am michela5, and I have learned how to develop a full-stack web app and deploy it!
+    </div>
+  );
+}
